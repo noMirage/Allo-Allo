a68e57aa57565d1be482fc1e5b5b1df17ea56c4c
andrii <andriiivanchov@gmail.com>
2026-02-10 18:05:13 +0300

    change http server

--- FrontendPart/src/utils/js/utilServer.ts
+++ FrontendPart/src/utils/js/utilServer.ts
@@ -16,7 +16,7 @@ export async function utilServer<T>(
 
   try {
     const res = await axios.request({
-      url: `http://localhost:8000/api${url}`,
+      url: `https://allo-allo.com.ua/api/${url}`,
       method,
       data: method !== 'get' ? data : undefined,
       headers,
